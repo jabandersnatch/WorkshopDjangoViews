697053f7e42e8bc551d52a75ed8ee751f2e08162
Juan Mendez <ja.mendez@uniandes.edu.co>
2022-08-28 20:22:10 -0500

    Views and urls done

--- measurements/views.py
+++ measurements/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from .logic import measurments_logic as ml
+from django.http import HttpResponse
+from django.core import serializers
+import json
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
+def measurements_view(request):
+    if request.method == 'GET':
+        id = request.GET.get('id',None)
+        if id:
+            measurment_dto = ml.get_measurement(id)
+            measurment = serializers.serialize('json', [measurment_dto,])
+            return HttpResponse(measurment, 'application/json')
+
+        else:
+            measurments_dto = ml.get_measurements()
+            measurments = serializers.serialize('json', measurments_dto)
+            return HttpResponse(measurments, 'application/json')
+
+    if request.method == 'POST':
+        measurment_dto = ml.create_measurement(json.loads(request.body))
+        measurment = serializers.serialize(json, [measurment_dto,])
+        return HttpResponse(measurment, 'application/json')
+@csrf_exempt
+def measurement_view(request, pk):
+    if request.method == 'GET':
+        measurment_dto = ml.get_measurement(pk)
+        measurment = serializers.serialize('json', [measurment_dto,])
+        return HttpResponse(measurment, 'application/json')
+
